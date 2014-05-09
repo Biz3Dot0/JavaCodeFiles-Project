7c06ed5a9a52ee7e54563e08ae92768780356c53
Muthukumar <office@gvmuthu.com>
2014-05-09 16:36:42 +0530

    Updated

--- MainActivity.java
+++ MainActivity.java
@@ -11,7 +11,7 @@ import android.view.View;
 
 public class MainActivity extends Activity {
 
-	// Updated Code
+	// Updated Code - For Customer
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
